11adb55306aa97875717dec968aa5529a3b44de6
Miguel Canessa <canessaalvamiguel@gmail.com>
2021-12-04 02:13:14 -0500

    Implementing akka http sample

--- build.sbt
+++ build.sbt
@@ -3,7 +3,11 @@ name := "ScalaSimpleSamples"
 version := "0.1"
 
 scalaVersion := "2.13.7"
+val akkaHttpVersion = "10.1.11"
+val akkaVersion = "2.5.26"
 
 libraryDependencies ++= Seq(
-  "net.liftweb" %% "lift-json" % "3.4.2"
+  "net.liftweb" %% "lift-json" % "3.4.2",
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion
 )
---
+++ src/main/scala/canessa/AkkaHttpSample.scala
@@ -0,0 +1,32 @@
+package canessa
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest, HttpResponse}
+import akka.stream.ActorMaterializer
+import scala.concurrent.duration._
+import scala.concurrent.Future
+
+object AkkaHttpSample extends App{
+
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  val request = HttpRequest(
+    method = HttpMethods.GET,
+    uri = "https://catfact4545.ninja/fact"
+  )
+
+  def sendRequest() = {
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(request)
+    val entityFuture: Future[HttpEntity.Strict] = responseFuture.flatMap(_.entity.toStrict(5.seconds))
+    entityFuture.map(_.data.utf8String)
+  }
+
+  sendRequest().map( x =>
+    println(x)
+  ).recover(
+    a => println("Something went wrong")
+  )
+}
